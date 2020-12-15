b00c1b8ce92005b8c5e37b1096d54c6be7804ce2
KLB <k.besser@tu-bs.de>
2020-12-15 15:07:10 +0100

    Fix logging if more than 2 v are present

--- clean_bib_file.py
+++ clean_bib_file.py
@@ -173,9 +173,9 @@ if __name__ == "__main__":
                         help="If this is set, the IDs of the bib entries are replaced by a fixed scheme")
     parser.add_argument("--arxiv", action="store_true",
                         help="If this is set, the primaryClasses are downloaded for arXiv preprints. Requires a eprint field in the entry")
-    parser.add_argument("-v", "--verbose", action="count", default=0)
+    parser.add_argument("-v", "--verbose", action="count", default=0, help="Verbosity level. -v is info and -vv is debug")
     args = vars(parser.parse_args())
-    args['verbose'] = logging.WARN - 10*args['verbose']
+    args['verbose'] = max([logging.WARN - 10*args['verbose'], logging.DEBUG])
     if args['arxiv']:
         import feedparser
     main(**args)
