0817d7b0210d183aa5fbb5dfc7e4acd121dd449c
KLB <k.besser@tu-bs.de>
2020-12-18 18:29:49 +0100

    Bump version to 0.2.0 and add changes to CHANGELOG

--- bibtextools/__init__.py
+++ bibtextools/__init__.py
@@ -1,11 +1,13 @@
 __author__ = "Karl-Ludwig Besser"
 __email__ = "k.besser@tu-bs.de"
-__version__ = "0.1.1"
+__version__ = "0.2.0"
 
 from . import clean_bib_file
+from . import modernize_bib_file
 
 
 __all__ = [
     "__version__",
     "clean_bib_file",
+    "modernize_bib_file"
     ]
