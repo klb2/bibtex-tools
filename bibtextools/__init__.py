b417cc9680bfc9393f631bccee3a78fb3b755687
KLB <k.besser@tu-bs.de>
2021-01-04 14:51:58 +0100

    Add combine bib files to __init__

--- bibtextools/__init__.py
+++ bibtextools/__init__.py
@@ -4,10 +4,12 @@ __version__ = "0.2.0"
 
 from . import clean_bib_file
 from . import modernize_bib_file
+from . import combine_bib_files
 
 
 __all__ = [
     "__version__",
     "clean_bib_file",
-    "modernize_bib_file"
+    "modernize_bib_file",
+    "combine_bib_files"
     ]
