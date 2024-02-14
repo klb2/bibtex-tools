919b34c8f3ac2003290c262362d0013adeca4c58
Karl Besser <kb1717@princeton.edu>
2024-02-13 19:29:04 -0500

    Bump version to 0.4.0 and update changelog

--- bibtextools/__init__.py
+++ bibtextools/__init__.py
@@ -1,6 +1,6 @@
 __author__ = "Karl-Ludwig Besser"
 __email__ = "karl.besser@princeton.edu"
-__version__ = "0.3.1"
+__version__ = "0.4.0"
 
 from . import clean_bib_file
 from . import modernize_bib_file
